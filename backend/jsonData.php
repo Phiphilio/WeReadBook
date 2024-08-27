a300d3eafc63823420e97e4ec4ba03295c4b25dd
Érudit Aoue <113186147+Phiphilio@users.noreply.github.com>
2024-08-27 19:43:14 +0200

    ajout de notes

--- backend/jsonData.php
+++ backend/jsonData.php
@@ -1,7 +1,10 @@
 <?php 
+/**pour que les éléments affichés par javascript gardent la même cohérence que ceux affiché depuis php
+j'ai préféré utiliser les informations de la base de donnée*/
 require_once(__DIR__ . "/connexionDb.php");
 //pour récupérer les données en json depuis mon script javascript
 //le echo permet de faire en sorte que le javascript interprète l'info
+//la fonction json_encode crée un tableau qui contient des objets correspondant à chaque sous tableau
 echo json_encode($listeLivre);
 /**
  * j'ai mis le "echo " pour pouvoir exécuter le fetch de mon javascript vers ici.
