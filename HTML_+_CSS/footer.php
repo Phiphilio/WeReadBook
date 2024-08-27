1068acc987e8415b6dda4705ef5941e608370e63
Érudit Aoue <113186147+Phiphilio@users.noreply.github.com>
2024-08-27 16:05:50 +0200

    on simplifie le code html

---
+++ HTML_+_CSS/footer.php
@@ -0,0 +1,34 @@
+<footer class="footer-container">
+    <div class="inner-footer-container">
+        <div class="footer-column">
+            <h3>abstract</h3>
+            <a href="#">Start trial</a>
+            <a href="#">Pricing</a>
+            <a href="#">Download</a>
+        </div>
+        <div class="footer-column">
+            <h3>abstract</h3>
+            <a href="#">Start trial</a>
+            <a href="#">Pricing</a>
+            <a href="#">Download</a>
+        </div>
+        <div class="footer-column">
+            <h3>abstract</h3>
+            <a href="#">Start trial</a>
+            <a href="#">Pricing</a>
+            <a href="#">Download</a>
+        </div>
+        <div class="footer-column">
+            <h3>abstract</h3>
+            <a href="#">Start trial</a>
+            <a href="#">Pricing</a>
+            <a href="#">Download</a>
+        </div>
+        <div class="copyright-footer-column">
+            <p>copyright 2024</br />
+                GrapperCosmo Studio, Inc<br />
+                All rights reserved</p>
+            <!--si je mets les <br/> entre des éléments en <p></p> il y aura un plus grand retour à la-->
+        </div>
+    </div>
+</footer>
--- HTML_+_CSS/index.php
+++ HTML_+_CSS/index.php
@@ -1,3 +1,7 @@
+<?php
+require_once(__DIR__ . "/../backend/connexionDb.php");
+
+?>
 <!DOCTYPE html>
 <html>
 
@@ -14,29 +18,14 @@
 </head>
 
 <body>
-  <header class="container">
-    <div class="navigation">
-      <div class="navTitle">
-        <a href="#">
-          <img
-            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAWBJREFUSEvFld9xgzAMxiUyROEhJmwSNiGTNJ0kdJKSSSDOA3QIo54dfHHxX3qXq19tfb9PsiwjvHjhi/UhChj7/gBZ1mSIJQEcF0MDEV1hntuiqoaQySBg5LxBgEtIgABOBWOt74wXMHH+BYZjBOhmgKsLiADtG2MnF8QJGG+3MyK+ywAzWJYLd7veJUREH0VZntd7FsAU0UHj/X5EIplRcJEQ1fpObMDT/ZAzVqWKr7PVTiyArr12P3FOMefGfpczVpvnXQAlKLsDhOh8NfdBc8Z+aboA8hIPG1ybR1VZYxmY7bmVEwd8c34hgGarsiqro1XtLkp4vT54EkAGT5z/5R6s8iyta/sxX3JqqZJfshbcmIXTvTcDubGMDNlRsZb1igcBiZCBhKhDf0L0w1Ggx7CT01V9OHLCzoifxX7fxe4oCWBko0Y1IdYp4tESme5WH5A11HyZJGcQK8W/AX4A0uGsGZifKx4AAAAASUVORK5CYII=" />
-          2i Academy</a>
-        <a href="#" id="HelpLink">gestion de la Bibliothèque</a>
-      </div>
-      <div class="navButton">
-        <section>
-          <button class="submitButton"> submit a request</button>
-          <button class="signButton"> sign in</button>
-        </section>
-      </div>
-    </div>
-  </header>
+  <!-- on récupre de le header-->
+  <?php require_once(__DIR__ . "/header.php") ?>
   <main>
     <section class=" middleTop container">
       <div class="searchBox">
         <div> quel livre cherchez vous ?</div>
         <form class="searchForm">
-          <input type="text" id ="searchInput"placeholder="Search">
+          <input type="text" id="searchInput" placeholder="Search">
           <button class="arrowButton">
             <img
               src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAKBJREFUSEvtlNEJgDAMRK+b6Ca6iZvoJrqJq7iJelBBVExy6p+F0p/yXnMJTfh4pY/5+AVmwkpEHYABwGTSgXAPCG8zvPZIohUUAMa1Ap6swJREBUwlJFEEIYkqcEvuBLNnSnZ3LnvytqA8PuppRD2AKk/UCU6ZKuAkmXBV4IYrghBcETQ5Gk7MZeZvNJn/EbdrqU12wZWI3ODt4l+BGdkCKy4hGU/4RpYAAAAASUVORK5CYII=" />
@@ -50,44 +39,12 @@
         <button id="sortDesc">Trier par date de sortie (Descendant)</button>
       </div>
       <div class="block-liste-item">
-        </div>
+      </div>
       </div>
     </section>
   </main>
-  <footer class="footer-container">
-    <div class="inner-footer-container">
-      <div class="footer-column">
-        <h3>abstract</h3>
-        <a href="#">Start trial</a>
-        <a href="#">Pricing</a>
-        <a href="#">Download</a>
-      </div>
-      <div class="footer-column">
-        <h3>abstract</h3>
-        <a href="#">Start trial</a>
-        <a href="#">Pricing</a>
-        <a href="#">Download</a>
-      </div>
-      <div class="footer-column">
-        <h3>abstract</h3>
-        <a href="#">Start trial</a>
-        <a href="#">Pricing</a>
-        <a href="#">Download</a>
-      </div>
-      <div class="footer-column">
-        <h3>abstract</h3>
-        <a href="#">Start trial</a>
-        <a href="#">Pricing</a>
-        <a href="#">Download</a>
-      </div>
-      <div class="copyright-footer-column">
-        <p>copyright 2024</br />
-          GrapperCosmo Studio, Inc<br />
-          All rights reserved</p>
-        <!--si je mets les <br/> entre des éléments en <p></p> il y aura un plus grand retour à la-->
-      </div>
-    </div>
-  </footer>
+  <!--on récupère le footer-->
+  <?php require_once(__DIR__ . "/footer.php") ?>
 </body>
 
 </html>
