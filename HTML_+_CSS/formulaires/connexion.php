8f86f8568dd92970ac6296106765cb309ae310d2
Érudit Aoue <113186147+Phiphilio@users.noreply.github.com>
2024-08-29 22:27:01 +0200

    mise en place de formulaire de connexion

---
+++ HTML_+_CSS/formulaires/connexion.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title> 2i tech Academy</title>
+  <link href="formStyles.css" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+    rel="stylesheet">
+  <script type="module" src="../JS/index.js" defer></script>
+</head>
+
+<body>
+  <!--on récupère le header-->
+  <?php require_once(__DIR__ . "/headerForm.php"); ?>
+  <main>
+    <div>
+      <h1 class="titre"> connexion </h1>
+      <div class="formConnexion">
+        <h3>Entrez vos informations</h3>
+        <form action="" method="post">
+          <label> adresse mail</label>
+          <input type="mail" name="email" placeholder="exemple@exemple.fr">
+          <label>mot de passe</label>
+          <input type="password" name="password">
+          <button>connexion</button>
+          <a href="inscription.php">inscription</a>
+        </form>
+      </div>
+    </div>
+  </main>
+</body>
---
+++ HTML_+_CSS/formulaires/inscription.php
@@ -0,0 +1,39 @@
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title> 2i tech Academy</title>
+  <link href="formStyles.css" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+    rel="stylesheet">
+  <script type="module" src="../JS/index.js" defer></script>
+</head>
+
+<body>
+<!--on récupère le header-->
+<?php require_once(__DIR__ ."/headerForm.php");?>
+<main>
+    <div>
+      <h1 class="titre"> inscription </h1>
+      <div class="formInscription">
+        <h3>Entrez vos informations</h3>
+        <form action="" method="post">
+          <label> adresse mail</label>
+          <input type="mail" name="email" placeholder="exemple@exemple.fr">
+          <label>mot de passe</label>
+          <input type="password" name="password">
+          <label>confirmez mot de passe</label>
+          <input type="password" name="password">
+          <button>inscription</button>
+          <a href="connexion.php">connexion</a>
+        </form>
+      </div>
+    </div>
+  </main>
+</body>
