cc1e67491c86f0fda183301c70d18393caefcf49
Moanahj <haji_mouna@hotmail.fr>
2024-09-04 14:19:21 +0200

    Modification apparence menu
    Menu burger
    Bouton d'interaction pour affichage menu à droite
    Modif css js

--- JS/script.js
+++ JS/script.js
@@ -96,6 +96,24 @@ fetch('./JS/livres.json')
     });
 });
 
+// Activation du menu burger 
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+
+function closeNav() {
+  sidenav.classList.remove("active");
+}
+
 // let galImages = document.querySelectorAll('lien-conteneur-photo');
 
 // for(let i = 0; i < galImages.length; i++){
