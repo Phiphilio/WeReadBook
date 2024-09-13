31d6c9fbd7064dd4db776950199a75dc8b969f5c
erudit <philaoue@gmail.com>
2024-09-13 20:59:06 +0200

    j'ai ajouté les metadonnees spécifiques au type de contenu envoyés

--- HTML_+_CSS/formulaires/inscriptionDonneesDB.php
+++ HTML_+_CSS/formulaires/inscriptionDonneesDB.php
@@ -11,48 +11,53 @@ $verificationMdp = trim($psotDonnees["verificationMdp"]);
 
 if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
     header('HTTP/1.1 400 bad Request');
-    echo json_encode("l'adresse email n'est pas valide");
+    // j'informe le côté client que la réponse sera au format json
+    //ce n'est pas indispensable, mais ça rend le tout plus clair pour le navigatuer
+    header('content-Type : application/json'); 
+    echo json_encode(["error"=>"l'adresse email n'est pas valide"]);
     exit();
 }
 
 foreach ($listeUser as $user) {
     if ($user["email"] === $postDonnees["email"]) {
         header('HTTP/1.1 403 Forbidden');
-        echo json_encode("cette adresse est deja utilise");
+        echo json_encode(["error"=>"cette adresse est deja utilise"]);
         exit();
     }
 }
 
 if (
-    empty($mdp) || empty($postDonnees["verificationMdp"])
+    empty($mdp) || empty($verificationMdp)
 ) {
-    echo json_encode("les deux champs doivent contenir le même mot de passe");
+    echo json_encode(["error"=>"les deux champs doivent contenir le même mot de passe"]);
     exit();
 }
 
-if ($mdp !== $psotDonnees["verificationMdp"]) {
+if ($mdp !== $verificationMdp) {
     header('HTTP/1.1 400 Bad Request');
-    echo json_encode("les deux mots de passe doivent correspondre");
+    echo json_encode(["error" =>"les deux mots de passe doivent correspondre"]);
     exit();
 }
 if (
     empty($nom) || strlen($nom) > 50
 ) {
     header('HTTP/1.1 400 Bad Request');
-    echo json_encode("entrez un nom d'utilisateur pas trop long");
+    echo json_encode(["error"=>"entrez un nom d'utilisateur pas trop long"]);
     exit();
 }
 
-if (strlen($postDonnees["mdp"]) < 6) {
+if (strlen($mdp) < 6) {
     header('HTTP/1.1 400 Bad Request');
-    echo json_encode("le mot de passe est trop petit");
+    echo json_encode(["error" =>"le mot de passe est trop petit"]);
     exit();
 }
 
-$inscriptionStatement = $mySql->prepare($requeteInscription);
+try {$inscriptionStatement = $mySql->prepare($requeteInscription);
 
 $inscriptionStatement->execute([
     ":nom" => htmlspecialchars($postDonnees["nom"]),
     ":email" => htmlspecialchars($postDonnees["email"]),
     ":motDePasse" => $postDonnees["mdp"],
 ]);
+
+}catch ()
